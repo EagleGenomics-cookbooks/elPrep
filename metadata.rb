ff4517b4dde02e5de05521904374aeb794356b5a
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2017-02-14 11:05:01 +0000

    Added source_url and issues_url

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'Apache v2.0'
 description 'Installs/Configures elPrep'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url 'https://github.com/EagleGenomics-cookbooks/elPrep'
+issues_url 'https://github.com/EagleGenomics-cookbooks/elPrep/issues'
 version '1.0.5'
 
 supports 'centos', '= 6.6'
